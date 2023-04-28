ff824cd1503cc9999d17e863e5f1a920a1d2dcfe
SoulSkunk <kylian.broccolichi@mediaschool.me>
2023-04-28 09:19:59 +0200

    back_finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/custom.scss";
+import "@/styles/global.scss";
+
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
